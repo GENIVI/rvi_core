43d243c1df5b8c204a3aff25328b6a254e66de15
Magnus <mfeuer@jaguarlandrover.com>
2014-07-26 15:39:01 -0700

    Added JLR copyright.
    Signed-off-by: Magnus <mfeuer@jaguarlandrover.com>

--- src/authorize_rpc.erl
+++ src/authorize_rpc.erl
@@ -1,3 +1,12 @@
+%%
+%% Copyright (C) 2014, Jaguar Land Rover
+%%
+%% This program is licensed under the terms and conditions of the
+%% Mozilla Public License, version 2.0.  The full text of the 
+%% Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
+%%
+
+
 -module(authorize_rpc).
 
 -export([handle_rpc/2]).
