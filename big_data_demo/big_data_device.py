6cb83a2916a760d805a3cc616092bdfa2ecbcebb
Magnus Feuer <mfeuer@jaguarlandrover.com>
2014-11-14 17:05:16 -0800

    Logger threading basic functionality seems to work. More debugging needed.

--- big_data_demo/big_data_device.py
+++ big_data_demo/big_data_device.py
@@ -27,17 +27,33 @@ from rvi_json_rpc_server import RVIJSONRPCServer
 import jsonrpclib
 import sqlite3
 from urlparse import urlparse
-
+import Queue
 
 MY_NAME = "GPS Collector"
+CMD_ADD_SUBSCRIPTION = 1
+CMD_DELETE_SUBSCRIPTION = 2
+CMD_ADD_SUBSCRIPTION = 3
+CMD_ADD_SAMPLE = 4
+CMD_RETRIEVE_NEXT_SAMPLE = 5
+CMD_DELETE_SAMPLE = 6
+CMD_DELETE_ALL_SAMPLES = 7
+CMD_DUMP_DATABASE = 8
+CMD_SHUTDOWN = 9
 
-class Logger:
-    def __init__(self, db_file = '/var/tmp/big_data_demo.sql'):
-        self.dbc = sqlite3.connect(db_file)
+class Logger(threading.Thread):
+    def __init__(self,db_file = '/var/tmp/big_data_demo.sql'):
+        threading.Thread.__init__(self)
+        self.db_file = db_file
+        self.queue = Queue.Queue()
+
+    # Sqlite commands can only be used from the same thread that
+    # created the database connection to begin with.
+    # Hence the stupid thread solution
+    def run(self):
+        self.dbc = sqlite3.connect(self.db_file)
         self.subscriptions = {}
 
-        print "Starting logger at {}".format(db_file)
-        print "SELF {}".format(self)
+        print "Starting logger at {}".format(self.db_file)
 
         # Create the table that stores log data and index it on its timestamps
         self.dbc.execute('''CREATE TABLE IF NOT EXISTS log (timestamp, channel, value)''')
@@ -53,11 +69,59 @@ class Logger:
             # Interval is the sample interval in sec. 
             # 0 is when the UTC of when last sample was made.
             self.subscriptions['channel'] = ( interval, 0 )
-            
-    def run(self):
+
         
+        while True:
+            # Try to get a command sent from a member function
+            # call invoked by another thread.
+            
+            elem = self.queue.get()
+            print "Elem:",elem
+            ( command, arg ) =  elem
+
+            
+            if command == CMD_ADD_SUBSCRIPTION:
+                (channel, sample_interval) = arg
+                self.__add_subscription(channel, sample_interval)
+
+            elif command == CMD_DELETE_SUBSCRIPTION:
+                self.__delete_subscription(arg)
+                    
+            elif command == CMD_ADD_SAMPLE:
+                (channel, value) = arg
+                self.__add_sample(channel, value)
+
+            elif command == CMD_RETRIEVE_NEXT_SAMPLE:
+                # Arg is a queue object to send back the result over
+                self.__retrieve_next_sample(arg)
+
+            elif command == CMD_DELETE_SAMPLE:
+                # Arg is timestamp to delete
+                self.__delete_sample(arg)
+
+            elif command == CMD_DELETE_ALL_SAMPLES:
+                self.__delete_all_sample()
+
+            elif command == CMD_DUMP_DATABASE:
+                self.__dump_db()
+
+            elif command == CMD_SHUTDOWN:
+                print "Logger:run(): Exiting thread"
+                return True
+
+            else:
+                print "Logger.run(): Unknown command: {} ignored".format(command)
+
+    
+        
+    def shutdown(self):
+        self.queue.put((CMD_SHUTDOWN, True))
+        self.join()
 
     def add_subscription(self, channel, sample_interval):
+        self.queue.put((CMD_ADD_SUBSCRIPTION, (channel, sample_interval)))
+
+    def __add_subscription(self, channel, sample_interval):
         if channel in self.subscriptions:
             print "Called {} already in subscriptions. Ignored".format(channel)
             return False
@@ -66,11 +130,7 @@ class Logger:
         # Setup a new channel in the dictionary
         self.subscriptions[channel] = (sample_interval, 0)
         try: 
-            print "1"
-            print sqlite3.complete_statement('''INSERT INTO subscriptions VALUES ('waypoint', 12);''')
-            print "1.2"
             self.dbc.execute('''INSERT INTO subscriptions VALUES (?, ?)''', (channel, sample_interval))
-            print "2"
             self.dbc.commit()
         except sqlite3.Error as e:
             print "An error occurred:", e.args[0]
@@ -79,6 +139,9 @@ class Logger:
         return True
 
     def delete_subscription(self, channel):
+        self.queue.put((CMD_DELETE_SUBSCRIPTION, channel))
+
+    def __delete_subscription(self, channel):
         if not channel in self.subscriptions:
             print "unsubscribe(): Channel {} not in subscriptions. Ignored".format(channel)
             return False
@@ -89,6 +152,9 @@ class Logger:
         return True
 
     def add_sample(self, channel, value):
+        self.queue.put((CMD_ADD_SAMPLE, (channel, value)))
+
+    def __add_sample(self, channel, value):
         # If the channel is not among our subscriptions, then ignore.
         print "add_sample({}): {}".format(channel, value)
         # [ind for ind, elem in enumerate(self.subscriptions) if v[0] == 53]
@@ -124,12 +190,19 @@ class Logger:
     #  False - no samples
     #  (timestamp, [ ( channel, value), ... ]) - Samples for the given timestamp
     #
-    def retrieve_next_sample():
+    def retrieve_next_sample(self):
+        q = Queue()
+        self.queue.put((CMD_RETRIEVE_NEXT_SAMPLE, True))
+        # Wait for a reply to come back and return whatever it was
+        return q.get()
+        
+    def __retrieve_next_sample(self, queue):
         # Get the oldest timestamp that we have stored.
         (ts, ) = dbc.execute('''SELECT min(timestamp) FROM log''').fetchone()
 
         # If no timestamp, then we have no data in db.
         if ts == None:
+            queue.put((False,))
             return False
             
         res = []
@@ -138,24 +211,34 @@ class Logger:
             # Convert value from string back to dict
             res.append(row[0], eval(row[1]))
 
-        return (ts, res)
+        queue.put((ts, res))
+        return True
 
-    # Delete samples with the given timestamp.
-    def delete_sample(timestamp):
-        dbc.execute('''DELETE FROM log WHERE timestamp=?''', (timestamp,))
+        
+    def delete_sample(self, timestamp):
+        self.queue.put((CMD_DELETE_SAMPLE, timestamp))
 
+    # Delete samples older than the given time stamp.
+    def __delete_sample(self, timestamp):
+        self.dbc.execute('''DELETE FROM log WHERE timestamp <= ?''', (timestamp,))
+
+    def delete_all_samples(self):
+        self.queue.put((CMD_DELETE_SAMPLE, True))
 
     # Delete allsamples with the given timestamp.
-    def delete_all_samples():
-        dbc.execute('''DELETE FROM log''')
+    def __delete_all_samples(self):
+        self.dbc.execute('''DELETE FROM log''')
 
-    def dump_db():
+    def dump_db(self):
+        self.queue.put((CMD_DUMP_DATABASE, True))
+
+    def __dump_db(self):
         print "LOG dump:"
-        for row in dbc.execute('''SELECT timestamp, channel, value FROM log'''):
+        for row in self.dbc.execute('''SELECT timestamp, channel, value FROM log'''):
             print row
         print "---"
         print "Subscription dump:"
-        for row in dbc.execute('''SELECT * FROM subscriptions'''):
+        for row in self.dbc.execute('''SELECT * FROM subscriptions'''):
             print row
         print "---"
     
@@ -165,10 +248,10 @@ class RVICallbackServer(threading.Thread):
     """
     
     def __init__(self, logger, service_edge, callback_url):
+        threading.Thread.__init__(self)
         self.logger = logger
         self.service_edge = service_edge
         self.callback_url = callback_url
-        threading.Thread.__init__(self)
         url = urlparse(self.callback_url)
         self.localServer =  RVIJSONRPCServer(addr=((url.hostname, url.port)), logRequests=False)
         self.register_services()
@@ -348,6 +431,8 @@ def cleanup(*args):
         gps_collector.shutdown()
     if rvi_callback:
         rvi_callback.shutdown()
+    if logger:
+        logger.shutdown()
     sys.exit(0)
 
 
