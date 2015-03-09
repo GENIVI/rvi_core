c8f3e1ba874c50d8172dc7f21942bcbf4042f6b7
Magnus Feuer <mfeuer@jaguarlandrover.com>
2015-03-09 15:54:51 -0700

    First, non-compilable stab

---
+++ components/schedule/src/rvi_schedule.erl
@@ -0,0 +1,24 @@
+%%
+%% Copyright (C) 2014, Jaguar Land Rover
+%%
+%% This program is licensed under the terms and conditions of the
+%% Mozilla Public License, version 2.0.  The full text of the 
+%% Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
+%%
+
+-module(rvi_schedule).
+
+-callback schedule_message(SvcName :: string(), 
+			   Timeout :: integer(),
+			   Callback :: mfa(), 
+			   Parameters :: any(), 
+			   Signature :: string(),
+			   Certificate :: string()).
+
+
+-callback register_remote_service(NetworkAddress :: string(), 
+				  AvailableServices :: string()).
+
+-callback unregister_remote_service(ServiceNames :: string()).
+
+
