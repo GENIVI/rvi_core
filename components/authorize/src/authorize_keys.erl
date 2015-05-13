bd5de2a5dbc700b40918cab267b80e2be0153426
Ulf Wiger <ulf@wiger.net>
2015-05-13 21:28:00 +0200

    JWT-style certificates, WIP

--- components/authorize/src/authorize_keys.erl
+++ components/authorize/src/authorize_keys.erl
@@ -1,9 +1,38 @@
 -module(authorize_keys).
 
 -export([get_key_pair/0]).
+-export([public_key_to_json/1,
+	 json_to_public_key/1]).
 
 -include_lib("lager/include/log.hrl").
 -include_lib("public_key/include/public_key.hrl").
+
+public_key_to_json(#'RSAPublicKey'{modulus = N, publicExponent = E}) ->
+    {struct, [{"kty", "RSA"},
+	      {"alg", "RS256"},
+	      {"use", "sig"},
+	      {"kid", "1"},
+	      {"e", base64url:encode(binary:encode_unsigned(E))},
+	      {"n", base64url:encode(binary:encode_unsigned(N))}
+	     ]}.
+
+json_to_public_key(JSON) ->
+    try
+	#'RSAPublicKey'{
+	   modulus = get_unsigned(["n"], JSON),
+	   publicExponent = get_unsigned(["e"], JSON)}
+    catch
+	throw:undefined ->
+	    undefined
+    end.
+
+get_unsigned(Path, JSON) ->
+    case rvi_common:get_json_element(Path, JSON) of
+	{ok, Value} ->
+	    binary:decode_unsigned(base64url:decode(Value));
+	{error, _} ->
+	    throw(undefined)
+    end.
 
 get_key_pair() ->
     case application:get_env(rvi, key_pair, undefined) of
