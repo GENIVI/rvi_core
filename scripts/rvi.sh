e59cc0af174952f6e598d99f36d7e3a9aaf1c7b8
Magnus Feuer <mfeuer@jaguarlandrover.com>
2015-11-18 16:42:22 -0800

    rvi_start.sh renamed rvi.sh.  Now accepts start|stop|console. Can now be run from any directory

--- scripts/rvi.sh
+++ scripts/rvi.sh
@@ -2,24 +2,32 @@
 #
 # Copyright (C) 2014, Jaguar Land Rover
 #
-# This program is licensed under the terms and conditions of the
 # Mozilla Public License, version 2.0.  The full text of the 
 # Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
 #
-
 #
 # Setup a correct configuration and launch an RVI release node.
 # If a UUID file has not been created, it will be done at this time.
+# Init.d script to start and stop an RVI system installed
+# through an RPM.
 #
 
 SELF_DIR=$(dirname $(readlink -f "$0"))
 CONFIG_DIR=/etc/opt/rvi
-BIN_DIR=/opt/rvi
+RVI_DIR=/opt/rvi
 
 usage() {
-    echo "Usage: $0 -c config_file"
-    echo "  -c config_file        Specify the configuration "
-    echo "Configuration data is read from the configuration file."
+    echo "Usage: $0 [-c config_file] start|stop|console"
+    echo "  -c config_file  Configuration file. "
+    echo "                  Not used with start and console."
+    echo
+    echo "  start           Start an rvi node with the given"
+    echo "                  configuration file."
+    echo
+    echo "  stop            Stop an rvi node previously started"
+    echo "                  with start."
+    echo
+    echo "  console         Start an rvi in foreground mode."
     exit 1
 }
 
@@ -34,8 +42,36 @@ while getopts "c:" o; do
     esac
 done
 
+shift $((${OPTIND}-1))
+CMD=$1
+
+if [ "$CMD" != "start" -a  "$CMD" != "stop" -a  "$CMD" != "console" ]
+then
+    usage
+fi
+
+if [ "$CMD" = "stop" ]
+then
+    ${RVI_DIR}/bin/rvi stop
+    exit $?
+fi
+
+# Check if we need to prepend current dir
+# to relative config file path
+#
+
+if [ $(echo ${CONFIG_FILE} | cut -c 1,1) != "/" ]
+then
+    CONFIG_FILE=${PWD}/${CONFIG_FILE}
+fi
+
 if [ -z "${CONFIG_FILE}" ] ; then
     echo "Missing -c flag"
+    usage
+fi
+
+if [ ! -f "${CONFIG_FILE}" ] ; then
+    echo "Config file cannot be read"
     usage
 fi
 
@@ -53,8 +89,8 @@ fi
 (
     cd /tmp/
     rm -rf rvi
-    export ERL_LIBS=${BIN_DIR}/setup:${BIN_DIR}/lib/
-    ${BIN_DIR}/setup_gen rvi $CONFIG_FILE rvi
+    export ERL_LIBS=${RVI_DIR}/setup:${RVI_DIR}/lib/
+    ${RVI_DIR}/setup_gen rvi ${CONFIG_FILE} rvi
 )
 
 # Did we succeed with config generation?
@@ -68,4 +104,4 @@ fi
 # which is symlinked to by /opt/rvi/sys.config
 cp /tmp/rvi/sys.config /etc/opt/rvi/sys.config
 
-exec /opt/rvi/bin/rvi start
+exec /opt/rvi/bin/rvi ${CMD}
